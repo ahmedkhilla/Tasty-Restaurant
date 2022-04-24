4d1beb716bbc1d06cb4da4be1d214278e2e9e03b
ahmedkhilla <ah.khilla97@gmail.com>
2022-04-24 15:27:07 +0300

    build opening hours section and why us section [html,css,responsive]

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
 //! Declarations
 const menuSection = document.querySelector('.menu-section');
 const closeBtn = document.querySelector('.close-icon');
-const menuBtn = document.querySelector('.burger-menu')
-const menuText = document.querySelector('.hidden-text')
+const menuBtn = document.querySelector('.burger-menu');
+const menuText = document.querySelector('.hidden-text');
+let preIcon = document.querySelector('.before-icon');
+let afterIcon = document.querySelector('.after-icon');
+// let slides = document.getElementsByClassName("slider");
+let slideIndex = 1;
+
 // console.log(menuBtn, closeBtn, menuSection);
 
 //! Nav-Menu Function 
 menuBtn.addEventListener('click', () => menuSection.classList.add('hidden'));
 menuText.addEventListener('click', () => menuSection.classList.add('hidden'))
-
 closeBtn.addEventListener('click', () => menuSection.classList.remove('hidden'));
+
+//! slider function
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slider");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "flex";
+}
+//! slider addEventListener
+preIcon.addEventListener('click', () => {
+  plusSlides(-1);
+})
+afterIcon.addEventListener('click', () => {
+  plusSlides(1)
+})
+
